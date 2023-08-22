238d26079f29e6459e258b791d08c8265a4cb475
Korbin Dansie <dansiekm@gmail.com>
2023-08-22 11:35:41 -0600

    Change URLs to accept request param

--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
 # Create your views here.
-def home_view(*args, **kwargs):
+def home_view(request, *args, **kwargs):
+    print(request.user) # Prints the user infomation in the console
     return HttpResponse("<h1>Hello World!</h1>")
+
+def contact_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Contact Page</h1>")
