b30b050e6470e96729b6b7e8096d266d880877bc
Korbin Dansie <dansiekm@gmail.com>
2023-08-22 17:34:24 -0600

    Product view from db

--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Product # .models is a relitave import
 
 # Create your views here.
+def product_detail_view(request):
+    obj = Product.objects.get(id=1)
+    context = {
+        "site_title": "Products",
+        "object": obj,
+    }
+    return render(request, "product/detail.html", context)
