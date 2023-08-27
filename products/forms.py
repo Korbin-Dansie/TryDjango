e275f0a195e565d62f1e051fc9767f92de854153
Korbin Dansie <dansiekm@gmail.com>
2023-08-27 17:50:20 -0600

    Added some validation to the form

--- products/forms.py
+++ products/forms.py
@@ -2,6 +2,15 @@ from django import forms
 from .models import Product
 
 class ProductForm(forms.ModelForm):
+    title = forms.CharField(label="Your Awesome Title") # Override what comes by default
+    description = forms.CharField(
+        required=False, 
+        widget=forms.Textarea(attrs={
+            "class": "new-class-name",
+            "rows": 10,
+            "placeholder": "Your description"
+        }))
+    
     class Meta:
         model = Product
         fields = [
@@ -10,6 +19,19 @@ class ProductForm(forms.ModelForm):
             'price'
         ]
 
+    # clean_<attribute>
+    def clean_title(self, *args, **kargs):
+        title = self.cleaned_data.get("title")
+        # Add validation(s) here for the title must pass all of them to pass
+        # Muse contain a vowel
+        print(len(title))
+        if len(title) < 4:
+            raise forms.ValidationError("Title must be longer than 3 characters")
+        else:
+            return title
+
+        
+# Used in views as an alternative way to create a form for products
 class RawProductForm(forms.Form):
     title = forms.CharField(label="Your Awesome Title")
     description = forms.CharField(
@@ -17,6 +39,6 @@ class RawProductForm(forms.Form):
         widget=forms.Textarea(attrs={
             "class": "new-class-name",
             "rows": 10,
-            "placeholder": "Your description"
+            "placeholder": "Your Other description"
         })) # Can set custom attributes here
     price = forms.DecimalField()
--- products/views.py
+++ products/views.py
@@ -6,29 +6,30 @@ from .forms import ProductForm, RawProductForm # Create a form using django mode
 # Create your views here.
 
 ## Using Django to autobuild form
-# def product_create_view(request):
-#     form = ProductForm(request.POST or None) #
-#     if form.is_valid():
-#         form.save()
-#         form = ProductForm() # Rerender the view after it is saved
+def product_create_view(request):
+    form = ProductForm(request.POST or None) # Sees if it is a POST request else initilize as empty
+    if form.is_valid():
+        form.save()
+        form = ProductForm() # Rerender the view after it is saved
+    context = {
+        "site_title": "Create Product",
+        "form": form,
+    }
+    return render(request, "products/product_create.html", context)
 
+## Using Django to build more complex form
+# def product_create_view(request):
+#     form = RawProductForm(request.POST or None) # Sees if it is a POST request else initilize as empty 
+#     if form.is_valid():
+#         Product.objects.create(**form.cleaned_data) # Placing the ** before it tells the model to treat from.cleaned_data as a dictionary of keyword arguments. Title = a, Price = 10 ...
+#         form = RawProductForm()
 #     context = {
 #         "site_title": "Create Product",
-#         "form": form,
+#         "form": form
 #     }
 #     return render(request, "products/product_create.html", context)
 
-## Using Django to build more complex form
-def product_create_view(request):
-    form = RawProductForm(request.POST or None) # Sees if it is a POST request else initilize as empty 
-    if form.is_valid():
-        Product.objects.create(**form.cleaned_data) # Placing the ** before it tells the model to treat from.cleaned_data as a dictionary of keyword arguments. Title = a, Price = 10 ...
-        form = RawProductForm()
-    context = {
-        "site_title": "Create Product",
-        "form": form
-    }
-    return render(request, "products/product_create.html", context)
+
 
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
